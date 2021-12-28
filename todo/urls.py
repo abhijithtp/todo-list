0cb22f97f10ecaffcec6b72fe6107cd21972a566
abhijithtp <atp260769@gmail.com>
2021-12-28 11:35:43 +0530

    modified3

--- todo/urls.py
+++ todo/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('',include('list.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$',serve,{'document_root':    settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
+   # url(r'^media/(?P<path>.*)$',serve,{'document_root':    settings.MEDIA_ROOT}),
+   #url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
 ]
