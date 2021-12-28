522de5ce9e3eb85a9f0706453757989ae9a6aa36
abhijithtp <atp260769@gmail.com>
2021-12-28 11:31:27 +0530

    modified2

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0c4hfaq)j03uuyv7(re#tja*2&4rrgtspa+u$p@b!sj7*qdygi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','todoatp.herokuapp.com']
 
 
 # Application definition
