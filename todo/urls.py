4ddc926ee20c6ec06f226a2ffb4beee05b864f26
abhijithtp <77284865+abhijithtp@users.noreply.github.com>
2021-12-28 12:30:55 +0530

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -19,10 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
-from todo.list import urls as list.urls
+from todo.list import urls as list_urls
 
 urlpatterns = [
-    path('',include('list.urls')),
+    path('',include(list_urls)),
     path('admin/', admin.site.urls),
    # url(r'^media/(?P<path>.*)$',serve,{'document_root':    settings.MEDIA_ROOT}),
    #url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
